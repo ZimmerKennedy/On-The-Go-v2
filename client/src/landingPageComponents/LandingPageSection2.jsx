4b43134a06d3e5fab03496ee7c5cd3edaa91eb3e
Zimmer Kennedy <kennedyzimmer00@gmail.com>
2023-05-08 10:52:20 -0800

    section-2

--- client/src/landingPageComponents/LandingPageSection2.jsx
+++ client/src/landingPageComponents/LandingPageSection2.jsx
@@ -3,14 +3,17 @@ import React from "react";
 const LandingPageSection2 = () => {
   return (
     <section className=" w-full px-20 mx-auto  min-h-screen pt-44">
-        <div className="text-text-color font-medium py-2 px-2 text-xl">How We Activate</div>
+        <div className="text-text-color font-medium py-2 px-2 text-xl">HOW WE ACTIVATE</div>
       <div className="flex flex-row justify-between items-start">
-        <div className="font-bruno-ace-sc text-7xl w-[80vw] tracking-tighter font-medium text-text-color">
+        <div className="font-bruno-ace-sc text-8xl w-[90vw] tracking-tighter font-medium text-text-color pb-16">
         STREAMLINED TICKET <span>MANAGEMENT SOLUTIONS</span>
         </div>
-        <div className="justify-self-end">01</div>
+        <div className="justify-self-end border-2 rounded-r-full rounded-l-full py-4 px-6 text-2xl text-text-color font-roboto">01</div>
       </div>
       <hr />
+      <div className="mt-16 font-roboto text-text-color text-5xl font-500 w-[39vw]">
+      A multicultural Travel Management company with an international presence of 33+ years and specialization in the marine & corporate travel industry.
+      </div>
     </section>
   );
 };
