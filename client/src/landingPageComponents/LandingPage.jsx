249337cd760cc0b3a62db1936e7871f841d6420a
Zimmer Kennedy <kennedyzimmer00@gmail.com>
2023-05-08 16:33:23 -0800

    added-framer

--- client/src/landingPageComponents/LandingPage.jsx
+++ client/src/landingPageComponents/LandingPage.jsx
@@ -1,17 +1,27 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import LandingPageSection1 from "./LandingPageSection1";
 import LandingPageSection2 from "./LandingPageSection2";
 
 import { Parallax } from "react-parallax";
 
-import { AnimatePresence } from "framer-motion";
+import { AnimatePresence, motion, useInView, useIsPresent, useAnimation } from "framer-motion";
 import Navbar from "../Navbar";
 import LandingPageSection3 from "./LandingPageSection3";
 import LandingPageSection4 from "./LandingPageSection4";
 
+
+
 const LandingPage = () => {
+  const ref = useRef(null);
+  const isInView = useInView(ref, {once: true});
+  const mainControls = useAnimation();
+  useEffect(() =>{
+    if(isInView){
+      mainControls.start("animate");
+    }
+  })
   return (
-    <main className="overflow-hidden bg-primary-color">
+    <main className="overflow-hidden bg-primary-color" ref={ref}>
       <Navbar />
       <LandingPageSection1 />
       <LandingPageSection2 />
--- client/src/landingPageComponents/LandingPageSection4.jsx
+++ client/src/landingPageComponents/LandingPageSection4.jsx
@@ -1,44 +1,55 @@
 import React from "react";
 import TransitioningBorder from "./TransitioningBorder";
-import { motion } from "framer-motion";
-import { borderAnimation, fadeAnimation } from "../config/motion";
+import { AnimatePresence, motion } from "framer-motion";
+import { slideAnimation, headContainerAnimation, fadeAnimation, borderAnimation } from "../config/motion";
+import { useInView } from "react-intersection-observer";
+
+
 const LandingPageSection4 = () => {
+  const [ref, inView] = useInView({
+    threshold: 0.2, 
+  });
   return (
-    <main className=" w-full px-20 mx-auto h-screen min-h-screen pt-40 relative flex flex-col">
-      <section class="grid grid-cols-2 gap-8">
+    <main ref={ref}className=" w-full px-20 mx-auto h-screen min-h-screen pt-40 relative flex flex-col">
+    {inView &&(
+
+        <section class="grid grid-cols-2 gap-8">
         <div className="why__title">WHY WE DO IT BETTER</div>
         <div className="why__title">
-          Antaeus Travel Group consists of a highly motivated and well
-          experienced team of passionate travel advisors, providing you holistic
-          travel services for almost any place in the world.
+        On The Go comprised of a dedicated and highly experienced team of enthusiastic travel experts. Committed to providing comprehensive travel services for destinations across the globe. 
         </div>
         <div class="why__description">
           <span className="why__description_num">1</span>
           International physical office network
-          
+          <motion.hr {...borderAnimation} className="my-6 border-t-2 z-[-1] w-full" />
         </div>
         <div class="why__description">
           <span className="why__description_num">2</span>
-          <p> 24/7 support all year round</p>
-          <motion.hr {...borderAnimation} className="my-6 h-24 w-full !important" />
+         24/7 support all year round
+          <motion.hr {...borderAnimation} className="my-6 border-t-2 z-[-1] w-full" />
         </div>
         <div class="why__description">
           <span className="why__description_num">3</span> Global availability &
           supplier management
+          <motion.hr {...borderAnimation} className="my-6 border-t-2 z-[-1] w-full" />
         </div>
         <div class="why__description">
           <span className="why__description_num">4</span> Tailor-made travel
           service
+          <motion.hr {...borderAnimation} className="my-6 border-t-2 z-[-1] w-full" />
         </div>
         <div class="why__description">
           <span className="why__description_num">5</span> Cost control
-          management{" "}
+          Management
+          <motion.hr {...borderAnimation} className="my-6 border-t-2 z-[-1] w-full" />
         </div>
         <div class="why__description">
           <span className="why__description_num">6</span> New digital travel
           management platform
+          <motion.hr {...borderAnimation} className="my-6 border-t-2 z-[-1] w-full" />
         </div>
       </section>
+    )}
     </main>
   );
 };
